4b8a2c3cb18534acb8d8b8d0b8a5d851ff0fcd28
Saleh <saleha@motory.com>
2024-10-31 08:58:38 +0300

    Typescript project boilerplate

--- controllers/home/homeController.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-export const getIndex = (req: Request, res: Response, next: NextFunction) => {
-  res.render('index', {
-    path: '/'
-  });
-};
--- routes/homeRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import * as homeController from '../controllers/home/homeController'
-
-const router = Router();
-
-router.get('/', homeController.getIndex);
-
-module.exports = router;
